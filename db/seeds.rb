799555dac34487f4cf2fa0c955a166f307150d34
Ruby Reed <rubysreed@gmail.com>
2021-11-16 17:02:09 -0700

    final submit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "seeds file running"
+
+require "faker"
+Person.destroy_all
+
+
+
+15.times do
+    Person.create(
+    full_name: Faker::Movies::HarryPotter.character,
+    age: Faker::Number.within(range: 1..60),
+    gender: Faker::Movies::HarryPotter.spell,
+    )
+    end
